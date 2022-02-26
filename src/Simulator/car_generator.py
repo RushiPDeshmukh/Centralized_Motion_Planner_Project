638463197fa22a73986a07a29b3290cca0f21e05
Harin Vashi <vashi.harin@gmail.com>
2022-02-26 15:20:32 -0500

    updated car_generator

--- src/Simulator/car_generator.py
+++ src/Simulator/car_generator.py
@@ -2,15 +2,16 @@ import numpy as np
 from car import *
 
 # used to randomly generate new cars
-def car_generator(node_list,interval):
+def car_generator(node_list,interval,number_cars):
     """node_list is a list of points where the cars can spawn or have destinations"""
     nodes = np.load('node_list.npy')
-    start_index = np.random.randint(0,len(nodes))
-    start_position = [nodes[start_index,0],nodes[start_index,1]]
-    goal_index = np.random.randint(0,len(nodes))
-    goal_position = [nodes[goal_index,0],nodes[goal_index,1]]
-    
-    t = 0
-    c1 = car(t,start_position,goal_position)
-    t = t + interval
-    c2 = car(t,start_position,goal_position)
+    ID = 0
+    car_list = []
+    for i in range(number_cars):
+        ID = ID + 1
+        start_index = np.random.randint(0,len(nodes))
+        start_position = [nodes[start_index,0],nodes[start_index,1]]
+        goal_index = np.random.randint(0,len(nodes))
+        goal_position = [nodes[goal_index,0],nodes[goal_index,1]]
+        car_list = car_list.append[car(t,start_position,goal_position,ID)]
+        t = t + interval
