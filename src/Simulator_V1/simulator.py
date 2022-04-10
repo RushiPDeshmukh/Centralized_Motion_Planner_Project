b835af6b81d05fb100534b21429c00a8909bc69e
wokeengineer <98420717+wokeengineer@users.noreply.github.com>
2022-04-10 18:27:10 -0400

    Simulator Node created

---
+++ src/Simulator_V1/simulator.py
@@ -0,0 +1,12 @@
+import pygame
+import numpy as np
+import pickle
+import os
+import random 
+
+
+
+"""
+Loads the map file and visualises the map.
+Starts a ROS node that reads the cars current node and updates the visual map to show the trajectory.
+"""
