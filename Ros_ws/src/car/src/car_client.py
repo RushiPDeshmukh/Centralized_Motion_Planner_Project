d9bad44ddeaa5186e9a065db23dbe03182dcb43c
malpurepiyush07@gmail.com <malpurepiyush07@gmail.com>
2022-04-16 17:20:05 -0400

    complete ros frame work

--- Ros_ws/src/car/src/car_client.py
+++ Ros_ws/src/car/src/car_client.py
@@ -1,30 +1,57 @@
 #!/usr/bin/env python3
 
 import sys
-from urllib import response
 import rospy
-from car.srv import *
+from std_msgs.msg import String
+from car.srv import planner_srv, planner_srvResponse
 #from car.msg import planner_msg, planner_msgResponse
+class car_ros:
+    def __init__(self):
+        """
+        Add ros init and stuff to generate car id if that is possible.
+        
+        
+        """
+        rospy.init_node('car_pub', anonymous=True)
+        #self.car_id = ros.init_node('car', anonymous=True)   ... IDK if this is what we will need lets see...
+        self.car_id = 0
+        pass
+        
+    def car_client(self, start, end):
+        rospy.wait_for_service('path_planner')
+        try:
+            self.path_planner = rospy.ServiceProxy('path_planner', planner_srv)
+            self.response = self.path_planner(start, end)
+            return self.response.path
+        except rospy.ServiceException as e:
+            print('service call failed : {}'.format(e))
 
-def car_client(start, end):
-    rospy.wait_for_service('path_planner')
-    try:
-        path_planner = rospy.ServiceProxy('path_planner', planner_srv)
-        response = path_planner(start, end)
-        return response.path
-    except rospy.ServiceException as e:
-        print('service call failed : {}'.format(e))
+    def car_publisher(self, x_pos, y_pos):
+        self.pub = rospy.Publisher('car_simulator', String, queue_size=10)
+        
+        rate = rospy.Rate(10) # 10hz
+        while not rospy.is_shutdown():
+            car_info = self.car_id + ',' + x_pos + ',' + y_pos 
+            self.pub.publish(car_info)
+            rate.sleep()
 
 if __name__ == "__main__":
     """
     TODO: Here take the numpy array and generate randomly the start and end points
+
+    The lines written below can should be deleted once similar lines are implemented in get_path() function in car.py file.
     """
     x1, y1 = [0, 0] # get this randomly
     xf, yf = [0, 0]
     start = [x1, y1]
     end = [xf, yf]
     print('Requesting')
-    path = car_client(start,end)
+    car = car_ros()
+    path = car.car_client(start,end)
     
     """TODO: Decide how to send this path further to the simulator"""
     """ 1. could just build the publisher subscriber right here, will check if I can make it possible."""
+
+
+
+    
--- Ros_ws/src/car/src/planner_server.py
+++ Ros_ws/src/car/src/planner_server.py
@@ -2,27 +2,31 @@
 
 from car.srv import planner_srv, planner_srvResponse
 import rospy
+class planner_ros:
+    def __init__(self):
+        rospy.init_node('planner_server_node')
+        
+        
+    def handle_start_end(self, req):
+        """
+        This is the planner server
+        This part of the code will handle the path planning
+        Returns a variable array planned_path which contains the path
 
-def handle_start_end(req):
-    """
-    This is the planner server
-    This part of the code will handle the path planning
-    Returns a variable array planned_path which contains the path
+        TODO: Planning algorithm implementation to be called here on receiving 
+        start and end node from the car as a request
+        access : req.start and req.end
+        start and end are a row 1x2 vector containg x,y coordinates
+        """
+        
+        self.planned_path =[]
+        return planner_srvResponse(self.planned_path)
 
-    TODO: Planning algorithm implementation to be called here on receiving 
-    start and end node from the car as a request
-    access : req.start and req.end
-    start and end are a row 1x2 vector containg x,y coordinates
-    """
-    
-    planned_path =[]
-    return planner_srvResponse(planned_path)
-
-def send_start_end():
-    rospy.init_node('planner_server_node')
-    s = rospy.Service('path_planner', planner_srv, handle_start_end)
-    print('The planner server is ready to plan')
-    rospy.spin()
+    def send_start_end(self):
+        s = rospy.Service('path_planner', planner_srv, self.handle_start_end)
+        print('The planner server is ready to plan')
+        rospy.spin()
 
 if __name__ == "__main__":
-    send_start_end()
+    planner = planner_ros()
+    planner.send_start_end()
