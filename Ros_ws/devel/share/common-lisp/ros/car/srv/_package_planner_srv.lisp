568e1bd9f5b33c90d1d52e57f52f190c7948c991
Piyush Malpure <malpurepiyush07@gmail.com>
2022-04-26 18:39:32 -0400

    Delete Ros_ws/devel directory

--- Ros_ws/devel/share/common-lisp/ros/car/srv/_package_planner_srv.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package car-srv)
-(cl:export '(START-VAL
-          START
-          END-VAL
-          END
-          PATH-VAL
-          PATH
-))
--- Ros_ws/devel/share/gennodejs/ros/car/srv/_index.js
+++
@@ -1,8 +0,0 @@
-
-"use strict";
-
-let planner_srv = require('./planner_srv.js')
-
-module.exports = {
-  planner_srv: planner_srv,
-};
