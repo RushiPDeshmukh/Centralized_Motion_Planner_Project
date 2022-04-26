0706c5c07dc37663bd621830b08a16f23d8f7496
wokeengineer <98420717+wokeengineer@users.noreply.github.com>
2022-04-26 19:11:29 -0400

    initial centralized planner.py

---
+++ src/Simulator_V1/centralized_planner.py
@@ -0,0 +1,18 @@
+import a_star 
+import  car_gen
+import time 
+import numpy as np
+
+def make_plan(start,goal,car_id = -1, t = 0, speed = 1):
+    car_id = 12
+    t = 12
+    speed = 1
+    path = a_star.main(start,goal)
+    path = np.tile(path,[1,2])
+    for i in range(len(path)):
+        path[i,3] = t+ i * speed 
+        path[i,2] = car_id
+
+    return path
+
+
