262812d7660f8cae8382b1576b2c681b99bc09c1
Harin Vashi <vashi.harin@gmail.com>
2022-04-29 20:35:54 -0400

    Added collision list option to A*

--- src/Simulator_V1/harins_tezt_file_2.py
+++ src/Simulator_V1/harins_tezt_file_2.py
@@ -1,6 +1,5 @@
 import numpy as np
 import car_gen
-import a_star
 from simulator import *
 import time
 from centralized_planner import *
@@ -12,7 +11,7 @@ path = dir_path + map_path
 file = open(path,'rb')
 rmap =  pickle.load(file)
 
-num_cars = 5
+num_cars = 10
 paths = np.empty((1,4))
 for i  in range(num_cars):
     s,g = car_gen.main()
@@ -28,7 +27,7 @@ paths = paths[paths[:,3].argsort()].astype(np.int16)
 for i in range(max(paths[:,3])):
     tuple_list = paths[paths[:,3]==i]
     render(win,rmap,tuple_list,car_list)
-    pygame.time.delay(100)
+    pygame.time.delay(500)
 
 # while simtime < 20:
 #     toc = time.time()
