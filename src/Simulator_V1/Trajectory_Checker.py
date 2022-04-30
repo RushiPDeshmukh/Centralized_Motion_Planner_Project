830e741e527e7aa2cf8777378ef014af16cbab2a
wokeengineer <98420717+wokeengineer@users.noreply.github.com>
2022-04-29 20:35:51 -0400

    updated trajectory file again!

--- src/Simulator_V1/Trajectory_Checker.py
+++ src/Simulator_V1/Trajectory_Checker.py
@@ -4,9 +4,10 @@ def is_valid(path,path_bank):
     collision_points = []
     validity = True
     for state in path:
-        if state in path_bank:
-            validity = False
-            collision_points.append(state)
+        for row in path_bank:
+            if row[0] == state[0] and row[1] == state[1] and row[3] == state[3]:
+                validity = False
+                collision_points.append(state)
     return validity,collision_points
 
 def correct_paths(path, path_bank):
@@ -14,4 +15,8 @@ def correct_paths(path, path_bank):
     curr_path = path
     while not valid:
         valid,collision_points = is_valid(curr_path,path_bank)
-        
+        for state in collision_points:
+            t = state[3]
+            path[path[:,3]>=t] = path[path[:,3]>=t] + 1
+            
+
