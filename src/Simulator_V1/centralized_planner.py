d9e9a58e8004a28872b40f41b5fcc9e24fa1ed9e
Harin Vashi <vashi.harin@gmail.com>
2022-04-30 13:22:26 -0400

    updated centralized planner

--- src/Simulator_V1/centralized_planner.py
+++ src/Simulator_V1/centralized_planner.py
@@ -1,33 +1,37 @@
+from Simulator_V1.Trajectory_Checker import correct_paths
 import a_star 
 import  car_gen
 import time 
 import numpy as np
 from Trajectory_Checker import is_valid
 
-def make_plan(start,goal,car_id = -1, t = 0, speed = 1,collisions = None, paths = None):
+def make_plan(start,goal,car_id = -1, t = 0, speed = 1, paths = None):
     #Wraps the car trajectory [x,y] into a tuple of [x,y,car_id,t]
     # paths = paths.reshape((-1,4))
-    oldpath = a_star.main(start,goal,collisions)
+    oldpath = a_star.main(start,goal)
     path = np.tile(oldpath,[1,2])
     for i in range(len(path)):
         path[i,3] = t + i * speed 
         path[i,2] = car_id
     # print('paths',paths)
-    v,c_p = is_valid(path,paths)
-    print("validity:",v)
-    print("collision points:",c_p)
+    valid,col_points = is_valid(path,paths)
+    print("validity:",valid)
+    print("collision points:",col_points)
     
-    if not v:
+    print('t+1 option')
+    path,valid,col_points = correct_paths(path,paths)
+    
+    while not valid:
+
         print('A* modified')
-        path = a_star.main(start,goal,c_p)
+        path = a_star.main(start,goal,col_points)
         path = np.tile(path,[1,2])
         for i in range(len(path)):
             path[i,3] = t + i * speed 
             path[i,2] = car_id
-        v,c_p = is_valid(path,paths)
-        print("validity after changing:",v,i)
+        valid,col_points = is_valid(path,paths)
+    print("Valid path found:",valid,i)
     
-   
     return path
 
 
