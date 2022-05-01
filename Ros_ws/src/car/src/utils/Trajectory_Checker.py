c20441cccd5fae662d7e958d5d1b3bcd8d2850cb
malpurepiyush07@gmail.com <malpurepiyush07@gmail.com>
2022-05-01 17:09:51 -0400

    new collision checker changes in ros

---
+++ Ros_ws/src/car/src/utils/Trajectory_Checker.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def is_valid(path,path_bank):
+    collision_points = []
+    validity = True
+    for state in path:
+        for row in path_bank:
+            # print('state',state)
+            if row[0] == state[0] and row[1] == state[1] and row[3] == state[3]:
+                validity = False
+                collision_points.append(list(state))
+    return validity,collision_points
+
+def correct_paths(path, path_bank):
+    valid = False
+    curr_path = path
+    count = 0
+    while not valid and count !=5:
+        valid,collision_points = is_valid(curr_path,path_bank)
+        count+=1
+        for state in collision_points:
+            t = state[3]
+            path[path[:,3]>=t] = path[path[:,3]>=t] + 1
+    return path,valid,collision_points
+    
+
--- Ros_ws/src/car/src/utils/centralized_planner.py
+++ Ros_ws/src/car/src/utils/centralized_planner.py
@@ -1,16 +1,41 @@
-import utils.a_star as a_star 
-import utils.car_gen as car_gen
+from Simulator_V1.Trajectory_Checker import correct_paths
+import a_star 
+import  car_gen
 import time 
 import numpy as np
+from Trajectory_Checker import is_valid
 
-def make_plan(start,goal,car_id = -1, t = 0, speed = 1):
-
-    path = a_star.main(start,goal)
-    path = np.tile(path,[1,2])
+def make_plan(start,goal,car_id = -1, t = 0, speed = 1, paths = None):
+    #Wraps the car trajectory [x,y] into a tuple of [x,y,car_id,t]
+    # paths = paths.reshape((-1,4))
+    oldpath = a_star.main(start,goal)
+    path = np.tile(oldpath,[1,2])
     for i in range(len(path)):
-        path[i,3] = t+ i * speed 
+        path[i,3] = t + i * speed 
         path[i,2] = car_id
-    #print(path)
+    # print('paths',paths)
+    valid,col_points = is_valid(path,paths)
+    print("validity:",valid)
+    print("collision points:",col_points)
+    
+    print('t+1 option')
+    path,valid,col_points = correct_paths(path,paths)
+    
+    while not valid:
+
+        print('A* modified')
+        path = a_star.main(start,goal,col_points)
+        path = np.tile(path,[1,2])
+        for i in range(len(path)):
+            path[i,3] = t + i * speed 
+            path[i,2] = car_id
+        valid,col_points = is_valid(path,paths)
+    print("Valid path found:",valid,i)
+    
     return path
+
+
+if __name__ == '__main__':
+    pass
 
 
