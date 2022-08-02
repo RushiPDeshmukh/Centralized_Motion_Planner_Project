3c7f041e3168dfa84ed7f511351ee4a8f1e621b8
wokeengineer <98420717+wokeengineer@users.noreply.github.com>
2022-08-02 15:33:10 -0400

    Final Edits

--- src/Simulator_V1/centralized_planner.py
+++ src/Simulator_V1/centralized_planner.py
@@ -20,17 +20,19 @@ def make_plan(start,goal,car_id = -1, t = 0, speed = 1, paths = None):
     # print('t+1 option')
     valid,col_points =is_valid(path,paths)
     if not valid:
+        c_p = col_points
         path,valid,col_points = correct_paths(path,paths)
         count = 0
-        while not valid and count<5 :
+        while not valid and count<15 :
             count +=1
             print('A* modified')
-            path = a_star.main(start,goal,col_points)
+            path = a_star.main(start,goal,c_p)
             path = np.tile(path,[1,2])
             for i in range(len(path)):
                 path[i,3] = t + i * speed 
                 path[i,2] = car_id
             valid,col_points = is_valid(path,paths)
+            c_p.append(col_points)
     return path
 
 
